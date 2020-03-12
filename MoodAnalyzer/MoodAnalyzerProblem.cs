0334a7254dfbe63302935252254eaf55f0eb864c
kneeraj825 <kneeraj825.com>
2020-03-12 11:09:58 +0530

    Added:Analyse and repond the happy or sad mood

---
+++ MoodAnalyzer/MoodAnalyzerProblem.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace MoodAnalyzer
+{
+    class MoodAnalyzerProblem
+    {
+        public String Mood(string message)
+        {
+            if (message.Contains("sad"))
+            {
+                return "SAD";
+            }
+            else
+            {
+                return "HAPPY";
+            }
+
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Mood Analyzer Problem");
+            MoodAnalyzerProblem moodanalyse = new MoodAnalyzerProblem();
+            moodanalyse.Mood("HAPPY");
+        }
+    }
+}
